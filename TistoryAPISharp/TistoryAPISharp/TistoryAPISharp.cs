2f104f34e815569700d9e3a80c620dec3556d7be
64bitm <64bitm@gmail.com>
2020-01-26 22:08:23 +0900

    파일 첨부를 제외한 상위 API 래핑 완료

--- TistoryAPISharp/TistoryAPISharp/TistoryAPISharp.cs
+++ TistoryAPISharp/TistoryAPISharp/TistoryAPISharp.cs
@@ -2,32 +2,41 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 #endregion
 
 namespace TistoryAPISharp {
     public class TistoryAPISharp {
-
-        #region Argument Class
-        private class Argument {
-            public string key { get; set; }
-            public string value { get; set; }
-        }
-        #endregion
-
         #region Enums
         public enum OutputStyle {
             XML = 0,
             JSON
         }
 
+        // 발행 상태 (0: 비공개, 1: 보호, 3: 발행)
+        public enum Visibillity {
+            Private = 0,
+            Protection,
+            Public
+        }
 
+        public enum acceptComment {
+            Allow = 1,
+            Deny = 0
+        }
         #endregion
 
         #region LocalVariable
-        private string ClientID;
-        private string AccessToken;
+        private string str_AccessTokenNotSet = "Access token not set. set access token first.";
+        private string str_ConnectionError = "Access token has expired or remote connection error.";
+        private string str_ModifyError = "Please check if the access torque is expired or out of connection, and the correct PostID.";
+
+        private string ClientID = string.Empty;
+        private string AccessToken = string.Empty;
         #endregion
 
         #region Get,Set Method
@@ -51,17 +60,17 @@ namespace TistoryAPISharp {
         // 블로그 정보
         public string GetBlogInformation(OutputStyle outputStyle) {
             try {
-                string output = string.Empty;
-                if (outputStyle == OutputStyle.JSON) {
-                    output = "&output=json";
+                if (string.IsNullOrEmpty(AccessToken)) {
+                    throw new Exception(str_AccessTokenNotSet);
                 }
-                string url = $"https://www.tistory.com/apis/blog/info?access_token={AccessToken}{output}";
+
+                string url = $"https://www.tistory.com/apis/blog/info?access_token={AccessToken}{GetOutputStyle(outputStyle)}";
                 return Get(url);
             }
             catch (Exception ex) {
                 switch (ex.HResult) {
                     case -2146233079:
-                    throw new Exception("Access token has expired or remote connection error.");
+                    throw new Exception(str_ConnectionError);
                 }
             }
             return string.Empty;
@@ -70,39 +79,144 @@ namespace TistoryAPISharp {
         // 글 목록
         public string GetPostList(OutputStyle outputStyle, string blogName, int pageNumber) {
             try {
-                string output = string.Empty;
-                if (outputStyle == OutputStyle.JSON) {
-                    output = "&output=json";
+                if (string.IsNullOrEmpty(AccessToken)) {
+                    throw new Exception(str_AccessTokenNotSet);
                 }
 
-                string url = $"https://www.tistory.com/apis/post/list?access_token={AccessToken}{output}&blogName={blogName}&page={pageNumber.ToString()}";
+                string url = $"https://www.tistory.com/apis/post/list?access_token={AccessToken}{GetOutputStyle(outputStyle)}&blogName={blogName}&page={pageNumber.ToString()}";
                 return Get(url);
             }
             catch (Exception ex) {
                 switch (ex.HResult) {
                     case -2146233079:
-                    throw new Exception("Access token has expired or remote connection error.");
+                    throw new Exception(str_ConnectionError);
                 }
             }
             return string.Empty;
         }
 
+        // 글 읽기
+        public string GetPostContent(OutputStyle outputStyle, string blogName, string postID) {
+            try {
+                if (string.IsNullOrEmpty(AccessToken)) {
+                    throw new Exception(str_AccessTokenNotSet);
+                }
 
+                string url = $"https://www.tistory.com/apis/post/read?access_token={AccessToken}{GetOutputStyle(outputStyle)}&blogName={blogName}&postId={postID}";
+                return Get(url);
+            }
+            catch (Exception ex) {
+                switch (ex.HResult) {
+                    case -2146233079:
+                    throw new Exception(str_ConnectionError);
+                }
+            }
+            return string.Empty;
+        }
+
+        // 글 쓰기
+        public string WritePost(OutputStyle outputStyle, string blogName, string title, string content, string tag = "", Visibillity visibillity = Visibillity.Private,
+            acceptComment acceptComment = acceptComment.Deny, string category = "0", string password = "",
+            string published = "", string slogan = "") {
+            try {
+                if (string.IsNullOrEmpty(AccessToken)) {
+                    throw new Exception(str_AccessTokenNotSet);
+                }
+
+                Dictionary<string, string> args = new Dictionary<string, string>();
+                args.Add("access_token", AccessToken);
+                args.Add("output", outputStyle == OutputStyle.JSON ? "json" : "");
+                args.Add("blogName", blogName);
+                args.Add("title", title);
+                args.Add("content", content);
+                args.Add("visibility", visibillity.ToString("D"));
+                args.Add("acceptComment", acceptComment.ToString("D"));
+                args.Add("category", category);
+                if (!string.IsNullOrEmpty(tag)) args.Add("tag", tag);
+                if (!string.IsNullOrEmpty(password)) args.Add("password", password);
+                if (!string.IsNullOrEmpty(published)) args.Add("published", published);
+                if (!string.IsNullOrEmpty(slogan)) args.Add("slogan", slogan);
+
+                return Post("https://www.tistory.com/apis/post/write", args);
+            }
+            catch (Exception ex) {
+                switch (ex.HResult) {
+                    case -2146233079:
+                    throw new Exception(str_ConnectionError);
+                }
+            }
+
+            return string.Empty;
+        }
+
+        // 글 수정
+        public string ModifyPost(OutputStyle outputStyle, string blogName, string PostID, string title, string content, string tag = "", Visibillity visibillity = Visibillity.Private,
+            acceptComment acceptComment = acceptComment.Deny, string category = "0", string password = "",
+            string published = "", string slogan = "") {
+            try {
+                if (string.IsNullOrEmpty(AccessToken)) {
+                    throw new Exception(str_AccessTokenNotSet);
+                }
+                
+                Dictionary<string, string> args = new Dictionary<string, string>();
+                args.Add("access_token", AccessToken);
+                args.Add("output", outputStyle == OutputStyle.JSON ? "json" : "");
+                args.Add("blogName", blogName);
+                args.Add("postId", PostID);
+                args.Add("title", title);
+                args.Add("content", content);
+                args.Add("visibility", visibillity.ToString("D"));
+                args.Add("acceptComment", acceptComment.ToString("D"));
+                args.Add("category", category);
+                if (!string.IsNullOrEmpty(tag)) args.Add("tag", tag);
+                if (!string.IsNullOrEmpty(password)) args.Add("password", password);
+                if (!string.IsNullOrEmpty(published)) args.Add("published", published);
+                if (!string.IsNullOrEmpty(slogan)) args.Add("slogan", slogan);
+
+                return Post("https://www.tistory.com/apis/post/modify", args);
+            }
+            catch (Exception ex) {
+                switch (ex.HResult) {
+                    case -2146233079:
+                    throw new Exception(str_ModifyError);
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public string AttatchFile(string blogName, string filePath) {
+            try {
+                Dictionary<string, string> args = new Dictionary<string, string>();
+                args.Add("access_token", AccessToken);
+                args.Add("blogName", blogName);
+
+                FileStream file = new FileStream(filePath, FileMode.Open);
+                return PostFileAsync("https://www.tistory.com/apis/post/attach", args, file).Result;
+            }
+            catch(Exception ex) {
+                switch (ex.HResult) {
+                    case -2146233079:
+                    throw new Exception(str_ConnectionError);
+                }
+                throw ex;
+            }
+        }
 
         #endregion
 
 
         #region Get,Post Method
-        private string Post(List<Argument> args, string Address) {
+        private string Post(string address, Dictionary<string, string> args) {
             try {
                 using (var client = new WebClient()) {
                     var values = new NameValueCollection();
 
                     foreach (var arg in args) {
-                        values[arg.key] = arg.value;
+                        values[arg.Key] = arg.Value;
                     }
 
-                    var response = client.UploadValues(Address, values);
+                    var response = client.UploadValues(address, values);
 
                     return Encoding.Default.GetString(response);
                 }
@@ -110,7 +224,27 @@ namespace TistoryAPISharp {
             catch (Exception ex) {
                 throw ex;
             }
+        }
 
+        private async Task<string> PostFileAsync(string address, Dictionary<string, string> args, FileStream file) {
+            try {
+                HttpClient httpClient = new HttpClient();
+                MultipartFormDataContent form = new MultipartFormDataContent();
+
+                foreach (var arg in args) {
+                    form.Add(new StringContent(arg.Key), arg.Value);
+                }
+
+                form.Add(new StreamContent(file), "uploadedfile");
+                HttpResponseMessage response = await httpClient.PostAsync(address, form);
+
+                response.EnsureSuccessStatusCode();
+                httpClient.Dispose();
+                return response.Content.ReadAsStringAsync().Result;
+            }
+            catch(Exception ex) {
+                throw ex;
+            }
         }
 
         private string Get(string address) {
@@ -122,10 +256,18 @@ namespace TistoryAPISharp {
             catch (Exception ex) {
                 throw ex;
             }
-
         }
         #endregion
 
+        #region Utility Functions
+        private string GetOutputStyle(OutputStyle outputStyle) {
+            string output = string.Empty;
+            if (outputStyle == OutputStyle.JSON) {
+                output = "&output=json";
+            }
+            return output;
+        }
+        #endregion
 
     }
 }
--- TistoryAPISharp/TistoryAPISharpTest/Program.cs
+++ TistoryAPISharp/TistoryAPISharpTest/Program.cs
@@ -11,16 +11,34 @@ namespace TistoryAPISharpTest {
         static void Main(string[] args) {
             TistoryAPISharp.TistoryAPISharp api = new TistoryAPISharp.TistoryAPISharp();
 
-            api.SetAccessToken("48c17c657fd9d6e8bab6e99e3e101cc3_bedbd619074209a19963092e0d4ad485");
+            api.SetAccessToken("cc7718ddca2f6e196f044eee7f6b57e2_ad89a4a9db58649ae87f7801a05748b3");
 
             // 블로그 정보 얻기
             //string result = api.GetBlogInformation(TistoryAPISharp.TistoryAPISharp.OutputStyle.JSON);
 
             // 글 목록
-            string reuslt = api.GetPostList(TistoryAPISharp.TistoryAPISharp.OutputStyle.JSON, "mos6502", 1);
+            //string reuslt = api.GetPostList(TistoryAPISharp.TistoryAPISharp.OutputStyle.JSON, "mos6502", 1);
 
-            Console.WriteLine(reuslt);
+            // 글 읽기
+            //string result = api.GetPostContent(TistoryAPISharp.TistoryAPISharp.OutputStyle.JSON, "mos6502", "2");
 
+            // 글 쓰기
+            //string result = api.WritePost(TistoryAPISharp.TistoryAPISharp.OutputStyle.JSON, "mos6502", "테스트 타이틀","테스트 컨텐츠", "테스트 태그, 테스트 태그 2");
+
+            // 글 수정
+            //string result = api.ModifyPost(TistoryAPISharp.TistoryAPISharp.OutputStyle.JSON, "mos6502", "49" ,"테스트 타이틀3","테스트 컨텐츠2", "테스트 태그1, 테스트 태그 3");
+
+            // 파일 첨부
+            string result = api.AttatchFile("mos6502", "C:\\Users\\64bitm\\Desktop\\nhkNews\\logo.jpg");
+
+            if (string.IsNullOrEmpty(result)) {
+                Console.WriteLine("Request Fail");
+            }
+            else {
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
         }
     }
 }
